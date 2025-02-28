51f6bda1b152890da05a292e0bb62f6e17e565b9
Hzadez <hzadez63@gmail.com>
2025-02-28 18:11:05 +0400

    Lapyenitema

--- index2.js
+++ index2.js
@@ -12,16 +12,17 @@ fetch(base_url2)
    <div class="card col-3" style="width: 18rem;">
    <div class="card-body">
    <h5 class="card-title">${datas.name}</h5>
-    <p class="card-text">${datas.premiered}</p>
-  </div>
-  <ul class="list-group list-group-flush" style="margin: 35px;">
-    <li class="list-group-item">IMDB Rating: ${datas.rating.average}</li>
-    <li class="list-group-item">Genre: ${datas.genres}</li>
-    <li class="list-group-item">Language: ${datas.language}</li>
+   </div>
+   <ul class="list-group list-group-flush" style="margin: 35px;">
+   <p class="card-text">IMDB Rating: ${datas.rating.average}</p>
+   <p class="card-text">Genre: ${datas.genres}</p>
+   <p class="card-text">Language: ${datas.language}</p>
+   <p class="card-text">Premiered: ${datas.premiered}</p>
+   <p class="card-text">Ended: ${datas.ended}</p>
   </ul>
   <div class="card-body" style="display: flex; width: max-content;margin: auto;">
     <a href="./index.html" class="btn btn-primary" style= "margin: 2px 2px;">Go to Website</a>
-     <a href="./index2.html?${datas.id}" class="btn btn-primary" style= "margin: 2px 2px; background-color: 198754;">Go to Detail</a> 
+    <a href="./index2.html?${datas.id}" class="btn btn-primary" style= "margin: 2px 2px; background-color: 198754;">Go to Detail</a> 
   </div>
 </div>`});
 })
